0d125bfda450a8ad7e25faec9b80547a256b5047
Simon Genette <simon.genette@ing.com>
2020-02-18 23:14:43 +0100

    Improved test

--- src/test/java/net/sgenette/springboottesting/library/db/BookRepositoryTest.java
+++ src/test/java/net/sgenette/springboottesting/library/db/BookRepositoryTest.java
@@ -1,16 +1,14 @@
 package net.sgenette.springboottesting.library.db;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class BookRepositoryTest {
 
@@ -19,12 +17,40 @@ public class BookRepositoryTest {
 
     @Test
     public void findBookById() {
-        Author author = Author.builder().id(1L).firstName("Robert").lastName("Martin").build();
-        Book book = Book.builder().id(1L).isbn(9780132350884L).title("Clean Code").author(author).build();
+        Book book = Book.builder().id(1L).isbn(9780132350884L).title("Clean Code").
+                author(Author.builder().id(1L).firstName("Robert").lastName("Martin").build()).
+                build();
 
         Optional<Book> actualBook = bookRepository.findById(1L);
 
         assertThat(actualBook).isPresent().hasValue(book);
+    }
+
+    @Test
+    public void deleteBook() {
+        Book book = Book.builder().id(1L).build();
+
+        bookRepository.delete(book);
+
+        assertAll(
+                () -> assertThat(bookRepository.count()).isEqualTo(2L),
+                () -> assertThat(bookRepository.findById(1L)).isEmpty()
+        );
+    }
+
+    @Test
+    public void addBook() {
+        Book book = Book.builder().title("Accelerate").isbn(9781942788331L).
+                author(Author.builder().firstName("Nicole").lastName("Forsgren").build()).
+                build();
+
+        bookRepository.save(book);
+        Optional<Book> actualBook = bookRepository.findById(4L);
+
+        assertAll(
+                () -> assertThat(bookRepository.count()).isEqualTo(4L),
+                () -> assertThat(actualBook).isPresent().hasValue(book)
+        );
     }
 
 }
