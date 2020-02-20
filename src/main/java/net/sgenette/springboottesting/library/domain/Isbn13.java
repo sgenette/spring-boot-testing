165d5281433219843a755a925951fe24427f6ede
Simon Genette <simon.genette@ing.com>
2020-02-20 22:19:59 +0100

    Added ISBN to domain

---
+++ src/main/java/net/sgenette/springboottesting/library/domain/Isbn13.java
@@ -0,0 +1,24 @@
+package net.sgenette.springboottesting.library.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.apache.commons.validator.routines.ISBNValidator;
+
+import java.util.Optional;
+
+@EqualsAndHashCode
+final class Isbn13 {
+
+    @Getter
+    private final String isbn13;
+
+    private static final Boolean CONVERT_TO_ISBN13 = Boolean.TRUE;
+
+    public Isbn13(String isbn) {
+        String validatedIsbn = ISBNValidator.getInstance(CONVERT_TO_ISBN13).validate(isbn);
+        this.isbn13 = Optional.ofNullable(validatedIsbn).orElseThrow(
+                () -> new IllegalArgumentException("Invalid ISBN")
+        );
+    }
+
+}
---
+++ src/test/java/net/sgenette/springboottesting/library/domain/Isbn13Test.java
@@ -0,0 +1,76 @@
+package net.sgenette.springboottesting.library.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
+
+public class Isbn13Test {
+
+    @Test
+    public void isbn10WithHyphens_shouldBeOk() {
+        String validIsbn10 = "0-19-853453-1";
+
+        Isbn13 actualIsbn13 = new Isbn13(validIsbn10);
+
+        assertThat(actualIsbn13.getIsbn13()).isInstanceOf(String.class).hasSize(13);
+    }
+
+    @Test
+    public void isbn10WithoutHyphens_shouldBeOk() {
+        String validIsbn10 = "0198534531";
+
+        Isbn13 actualIsbn13 = new Isbn13(validIsbn10);
+
+        assertThat(actualIsbn13.getIsbn13()).isInstanceOf(String.class).hasSize(13);
+    }
+
+    @Test
+    public void isbn13WithHyphens_shouldBeOk() {
+        String validIsbn13 = "978-1-56619-909-4";
+
+        Isbn13 actualIsbn13 = new Isbn13(validIsbn13);
+
+        assertThat(actualIsbn13.getIsbn13()).isInstanceOf(String.class).hasSize(13);
+    }
+
+    @Test
+    public void isbn13WithoutHyphens_shouldBeOk() {
+        String validIsbn13 = "9781566199094";
+
+        Isbn13 actualIsbn13 = new Isbn13(validIsbn13);
+
+        assertThat(actualIsbn13.getIsbn13()).isInstanceOf(String.class).hasSize(13);
+    }
+
+    @Test
+    public void invalidIsbn_shouldThrowIllegalArgumentException() {
+        String invalidIsbn = "12345678901";
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Isbn13(invalidIsbn)
+        );
+    }
+
+    @Test
+    public void emptyIsbn_shouldThrowIllegalArgumentException() {
+        String invalidIsbn = "";
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Isbn13(invalidIsbn)
+        );
+    }
+
+    @Test
+    public void nullIsbn_shouldThrowIllegalArgumentException() {
+        String invalidIsbn = null;
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Isbn13(invalidIsbn)
+        );
+    }
+
+}
