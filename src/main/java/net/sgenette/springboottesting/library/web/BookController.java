1c07f317c3e8a876e8bdebe24cf9613cf58d55c0
Simon Genette <simon.genette@ing.com>
2020-03-04 23:45:59 +0100

    Added simple REST controler for books

---
+++ src/main/java/net/sgenette/springboottesting/library/web/BookController.java
@@ -0,0 +1,25 @@
+package net.sgenette.springboottesting.library.web;
+
+import net.sgenette.springboottesting.library.core.Book;
+import net.sgenette.springboottesting.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> books() {
+        return bookService.getAllBooks();
+    }
+}
---
+++ src/test/java/net/sgenette/springboottesting/library/web/BookControllerTest.java
@@ -0,0 +1,54 @@
+package net.sgenette.springboottesting.library.web;
+
+import net.sgenette.springboottesting.library.core.Author;
+import net.sgenette.springboottesting.library.core.Book;
+import net.sgenette.springboottesting.library.core.Isbn13;
+import net.sgenette.springboottesting.library.core.Name;
+import net.sgenette.springboottesting.library.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void books() throws Exception {
+        Book aBook = Book.builder().
+                isbn13(new Isbn13("978 1 86197 876 9")).
+                title("Clean code").
+                author(Author.builder().
+                        firstName(new Name("Robert")).
+                        lastName(new Name("Martin")).
+                        build()).
+                build();
+        List<Book> books = Arrays.asList(aBook);
+
+        Mockito.when(bookService.getAllBooks()).thenReturn(books);
+
+        mvc.perform(get("/books")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].isbn13.isbn13", is(aBook.getIsbn13().getIsbn13())));
+    }
+}
