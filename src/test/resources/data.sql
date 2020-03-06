f6962ea29af6b68ecc6f94e801cbce7733eb5aba
Simon Genette <simon.genette@ing.com>
2020-03-06 21:11:30 +0100

    Removed test data

--- src/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO authors (first_name, last_name) VALUES
-('Robert', 'Martin'),
-('Kent', 'Beck'),
-('Martin', 'Fowler');
-
-INSERT INTO books (isbn, title, author_id) VALUES
-(9780132350884, 'Clean Code', 1),
-(9780321278654, 'Extreme Programming Explained', 2),
-(9780134757599, 'Refactoring', 3);
