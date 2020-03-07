10d5e1b91241cd1b8c6ed76c3eeeebdfd8a91f5b
Simon Genette <simon.genette@ing.com>
2020-03-07 15:37:55 +0100

    Improved tests

--- src/test/java/net/sgenette/springboottesting/library/mapper/AuthorDTOMapperTest.java
+++ src/test/java/net/sgenette/springboottesting/library/mapper/AuthorDTOMapperTest.java
@@ -2,25 +2,27 @@ package net.sgenette.springboottesting.library.mapper;
 
 import net.sgenette.springboottesting.library.web.dto.AuthorDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static net.sgenette.springboottesting.library.model.data.AuthorData.MARTIN_FOWLER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SpringBootTest
 public class AuthorDTOMapperTest {
 
-    @Autowired
-    AuthorDTOMapper mapper;
+    private AuthorDTOMapper mapper;
+
+    public AuthorDTOMapperTest() {
+        this.mapper = new AuthorDTOMapperImpl(new NameMapper());
+    }
 
     @Test
     public void author_shouldMapToAuthorDTO() {
         AuthorDTO actualAuthorDTO = mapper.authorToAuthorDTO(MARTIN_FOWLER);
 
-        assertThat(actualAuthorDTO).isNotNull();
-        assertThat(actualAuthorDTO.getFirstName()).isEqualTo("Martin");
-        assertThat(actualAuthorDTO.getLastName()).isEqualTo("Fowler");
+        assertAll(
+                () -> assertThat(actualAuthorDTO).isNotNull(),
+                () -> assertThat(actualAuthorDTO.getFirstName()).isEqualTo("Martin"),
+                () -> assertThat(actualAuthorDTO.getLastName()).isEqualTo("Fowler"));
     }
 
     @Test
--- src/test/java/net/sgenette/springboottesting/library/mapper/BookDTOMapperTest.java
+++ src/test/java/net/sgenette/springboottesting/library/mapper/BookDTOMapperTest.java
@@ -2,27 +2,30 @@ package net.sgenette.springboottesting.library.mapper;
 
 import net.sgenette.springboottesting.library.web.dto.BookDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static net.sgenette.springboottesting.library.model.data.BookData.CLEAN_CODE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SpringBootTest
 public class BookDTOMapperTest {
 
-    @Autowired
     private BookDTOMapper mapper;
+
+    public BookDTOMapperTest() {
+        this.mapper = new BookDTOMapperImpl(new NameMapper(), new Isbn13Mapper());
+    }
 
     @Test
     public void book_shouldMapToBookDTO() {
         BookDTO actualBookDTO = mapper.bookToBookDTO(CLEAN_CODE);
 
-        assertThat(actualBookDTO).isNotNull();
-        assertThat(actualBookDTO.getIsbn()).isEqualTo("9781861978769");
-        assertThat(actualBookDTO.getTitle()).isEqualTo("Clean code");
-        assertThat(actualBookDTO.getAuthor().getFirstName()).isEqualTo("Robert");
-        assertThat(actualBookDTO.getAuthor().getLastName()).isEqualTo("Martin");
+        assertAll(
+                () -> assertThat(actualBookDTO).isNotNull(),
+                () -> assertThat(actualBookDTO.getIsbn()).isEqualTo("9781861978769"),
+                () -> assertThat(actualBookDTO.getTitle()).isEqualTo("Clean code"),
+                () -> assertThat(actualBookDTO.getAuthor()).isNotNull(),
+                () -> assertThat(actualBookDTO.getAuthor().getFirstName()).isEqualTo("Robert"),
+                () -> assertThat(actualBookDTO.getAuthor().getLastName()).isEqualTo("Martin"));
     }
 
     @Test
